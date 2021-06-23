c634db7907c939b8428b6d4a5d4c23bb2c015d64
Antonio Pontes <anvictorsantos@gmail.com>
2021-06-23 19:48:10 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <comments></comments>
+</template>
+
+<script>
+import Comments from './components/Comments.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Comments
+  }
+}
+</script>
